24e3f90d3af850f24f395cbad535cb4604068619
Jan Dejmal <xdejma01@vutbr.cz>
2022-10-26 19:09:47 +0200

    doplneni kodu + komentare

--- komprimace.py
+++ komprimace.py
@@ -1,24 +1,51 @@
 import os
 import gzip
 import shutil
-#TODO vytvorit slozku, kam se budou ukladat .gz
-# doplnit readme
 
 #Declare a path to log files
-path = "var/log/"
-#load contents of log direcotory
-files = os.listdir(path)
+PATH = "var/log/"
+#First two bytes in gzip
+GZIP_NUMBER = b'\x1f\x8b'
+#Load contents of log direcotory
+files = os.listdir(PATH)
+
+def is_gzip(file_name):
+    """ 
+    Function that checks if a file is already gzipped
+    Opens the file and checks first two bytes, then compares them with expected bytes that are always at the begining of gzip
+    
+    Args:
+        file_name (str): Name of the checked file
+    Returns:
+        float: Ture if first two bytes from opened file are equals to GZIP_NUMBER
+    """
+    f = open(file_name, 'rb')
+    read = f.read(2)
+    f.close()
+    return read == GZIP_NUMBER
 
 for i in files:
-    if not os.path.isdir(path + i):
+    """ 
+    Loop checks every item in the list "files"
+    Item can't be a directory and it can't be a gzip
+    Gzip file is created, then the content from original log file is copied to the new gzip file and compressed
+    At the end of each iteration, loop removes original uncompressed log file
+    """
+    file_path = PATH + i
+    
+    if not os.path.isdir(file_path) and not is_gzip(file_path):
+        
         #open original file to read and create new compressed file
-        f_original = open(path + i, 'rb')
-        f_compressed = gzip.open(path + i + ".gz", 'wb')
+        f_original = open(file_path, 'rb')
+        f_compressed = gzip.open(file_path + ".gz", 'wb')
+        
         #copy from original to compressed 
         shutil.copyfileobj(f_original, f_compressed)
+        
         #close both files
         f_original.close()
         f_compressed.close()
-        #remove non-compressed files
-        os.remove(path + i)
         
+        #remove non-compressed files
+        os.remove(file_path)
+
